20152cc6110e713aa4d68491ea7bba9ba9412f33
Nicholas Silva <42357335+flexportnes@users.noreply.github.com>
2018-12-06 15:00:36 -0800

    Disable Test Adapter Raises (#2)

--- lib/active_job/limiter/queue_adapters/test_adapter.rb
+++ lib/active_job/limiter/queue_adapters/test_adapter.rb
@@ -4,14 +4,12 @@ module ActiveJob
   module Limiter
     module QueueAdapters
       module TestAdapter
-        class MethodNotStubbedError < StandardError; end
-
         def self.check_lock_before_enqueue(_job, _expiration)
-          raise MethodNotStubbedError, 'This method must be stubbed in tests'
+          true
         end
 
         def self.clear_lock_before_perform(_job)
-          raise MethodNotStubbedError, 'This method must be stubbed in tests'
+          true
         end
       end
     end
